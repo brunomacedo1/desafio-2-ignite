70eec48bc4fa4f145713c9cd4af0a3271c837101
Macedo <brunomacedo_31@hotmail.com>
2021-07-11 12:57:11 -0300

    Desafio 2 concluido

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { useEffect } from "react";
+import { api } from "../services/api";
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Content({movies, selectedGenre}: ContentProps) {
+
+  return (
+    <div className="container">
+      <Header selectedGenre={selectedGenre.title}/>
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
